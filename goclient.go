ac210589af22b4d0a4d92b3986a1e353310ec24e
Aishah Faisal <aishah57@gmail.com>
2017-06-20 02:15:49 +0800

    Add files via upload

---
+++ goclient.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("\n\t -t,		total RAM in kilobytes of the server \n")
+	fmt.Print("\t -u,		free RAM in kilobytes of the server \n")
+	fmt.Print("\t -f,		used RAM in kilobytes of the server \n")
+
+	for {
+		fmt.Print("\n Choose your command: ")
+		text, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, text + "\n")
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("\nResults from server: \n\t" + message + "\n")
+	}
+}
